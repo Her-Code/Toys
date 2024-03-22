28ff9f16c252c8b40dd3e7ff964d3744a550024e
+Sharon <+wanjiku.kahira@gmail.com>
2024-03-22 06:15:05 +0300

    Create an index.js file

---
+++ gradeGenerator/index.js
@@ -0,0 +1,19 @@
+function calGrade(){
+    const marks = parseFloat(document.getElementById('marks').value);
+
+let grade;
+
+if (marks > 79){
+    grade = 'A' ;
+}else if(marks >= 60){
+    grade = 'B' ;
+}else if (marks >= 50){
+    grade = 'C'
+}else if(marks >= 40){
+    grade = 'D' ;
+}else{
+    grade = 'E'
+}
+
+document.getElementById('result').innerText = 'Student grade: ${grade}';
+}
