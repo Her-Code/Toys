dcfe55770b6b15af75bedb4fa7ec2ae952c23216
+Sharon <+wanjiku.kahira@gmail.com>
2024-03-23 01:20:29 +0300

    Add functions to js file

---
+++ speedDetector/index.js
@@ -0,0 +1,19 @@
+function checkSpeed(){
+    const speedInput =document.getElementById("speed").value;
+    //coversion of input value to a number.
+    let speed = parseInt(speedInput)
+
+    if(isNaN(speed) || speed < 0){
+        document.getElementById("result").textContent = 0;
+        return;
+    }
+    let demeritPoints = Math.floor((speed-70)/5);
+    if (speed <= 70){
+        document.getElementById("result").textContent = "Ok";
+    }else if (demeritPoints >= 12){
+        document.getElementById("result").textContent = "License suspended";
+    }else{
+        document.getElementById("result").textContent = "Points: " + demeritPoints ;
+    }
+
+}
