f5909ee03b34c739e2f699137c73a10aaba2f120
+Sharon <+wanjiku.kahira@gmail.com>
2024-03-23 12:28:14 +0300

    fix issues

--- gradeGenerator/index.js
+++ gradeGenerator/index.js
@@ -2,24 +2,37 @@
 function calculateGrade(){
 
     // const marks = parseFloat(document.getElementById('marks').value);
-    let html =document.getElementById("html").value;
-        css =document.getElementById("css").value;
-        js =document.getElementById("js").value;
-        skill =document.getElementById("skill").value;
 
-    let sum = parseFloat(html) + parseFloat(css) + parseFloat(js) + parseFloat(skill)
-   
+    
+    let css =document.getElementById("css").value;
+    let js =document.getElementById("js").value;
+    let html =document.getElementById("html").value;
+    let skill =document.getElementById("skill").value;
+
+    let sum = parseFloat(html) + parseFloat(css) + parseFloat(js) + parseFloat(skill);
+
+    document.getElementById("demo").innerHTML =`Total mark is =${sum}`;
+
+    let percentage = (sum/400*100);
+
+    document.getElementById("demo").innerHTML = `Percentage is = ${percentage}`;
+
 let grade;
 
-if (marks > 79){
+if (percentage > 79){
+    document.getElementById("demo2").innerHTML = `Excellent`;
     grade = 'A' ;
-}else if(marks >= 60){
+}else if(percentage >= 60){
+    document.getElementById("demo2").innerHTML = `Very good`;
     grade = 'B' ;
-}else if (marks >= 50){
+}else if (percentage >= 50){
+    document.getElementById("demo2").innerHTML = `Good`;
     grade = 'C'
-}else if(marks >= 40){
+}else if(percentage >= 40){
+    document.getElementById("demo2").innerHTML = `Need hardwork`;
     grade = 'D' ;
 }else{
+    document.getElementById("demo2").innerHTML = `Fail`;
     grade = 'E'
 }
 
