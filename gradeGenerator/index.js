f306c0fa689f05293948a37bb47f6da220383189
+Sharon <+wanjiku.kahira@gmail.com>
2024-03-23 12:54:34 +0300

    add comments

--- gradeGenerator/index.js
+++ gradeGenerator/index.js
@@ -1,24 +1,29 @@
-
+//call  function
 function calculateGrade(){
 
     // const marks = parseFloat(document.getElementById('marks').value);
 
-    
+    // declare variables
     let css =document.getElementById("css").value;
     let js =document.getElementById("js").value;
     let html =document.getElementById("html").value;
     let skill =document.getElementById("skill").value;
-
+ //calculate sum
     let sum = parseFloat(html) + parseFloat(css) + parseFloat(js) + parseFloat(skill);
 
+// return sum
     document.getElementById("demo").innerHTML =`Total mark is =${sum}`;
 
+//declare percentage and ensure it can be reassigned
     let percentage = (sum/400*100);
 
+//return percentage
     document.getElementById("demo").innerHTML = `Percentage is = ${percentage}`;
 
+//introduce grade by declaring it
 let grade;
 
+//conditional statement min=0 max=100
 if (percentage > 79){
     document.getElementById("demo2").innerHTML = `Excellent`;
     grade = 'A' ;
@@ -36,6 +41,7 @@ if (percentage > 79){
     grade = 'E'
 }
 
+//return grade
 document.getElementById('demo').innerText = `Student grade: ${grade}`;
 }
 
